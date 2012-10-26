0eea77c7e488c01cf229f9529ed05f4f959241eb
Facundo Casco <facundo.casco@fierro-soft.com.ar>
2012-10-26 20:02:50 -0300

    refactoring: cree una clase para los canales de mensajes (una para archivos y otra para emails)

--- tests/ftp2email-tests.py
+++ tests/ftp2email-tests.py
@@ -13,30 +13,83 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), os.
 import utils.ftp2email as ftp2email
 
 
-class Ftp2emailTestCase(unittest.TestCase):
-    "Test para el envio de mensajes sinli por email desde archivos"
+class LoggingTester(object):
+
+    def __init__(self):
+
+        self.log_levels = ('debug', 'info', 'warning', 'error', 'critical')
+
+        self.flush()
+
+        # agregar un metodo por cada nivel de log
+        # que acumula los mensajes en el diccionario
+        map(lambda l: setattr(self, l, lambda x: self.messages[l].append(x)),
+            self.log_levels)
+
+    def flush(self):
+        self.messages = dict((x, []) for x in self.log_levels)
+
+
+ftp2email.logging = LoggingTester()
+
+
+class FilesystemChannelTestCase(unittest.TestCase):
+    "Test para el manejo de mensajes por archivos"
 
     @classmethod
     def setUpClass(cls):
         """Los archivos para tests estan en un directorio 'data'
             dentro del directorio donde está el test
         """
-        cls.script_path = os.path.dirname(os.path.realpath(__file__))
-        ftp2email.settings['base_path'] = os.path.join(cls.script_path, 'data')
+        cls.test_path = os.path.dirname(os.path.realpath(__file__))
 
     def setUp(self):
-        pass
+        self.ch = ftp2email.FilesystemChannel(os.path.join(self.test_path, 'data'),
+                                                '/L0002349_[A-Z][0-9]{7}$')
+        ftp2email.logging.flush()
 
     def test_get_message_files(self):
         "Verifica que se reconozcan correctamente los archivos con mensajes"
 
-        espected_files = [os.path.join(self.script_path, x)
+        espected_files = [os.path.join(self.test_path, x)
                             for x in ['data/L0002349_E0000001/vacio.xml',
                                       'data/L0002349_E0000001/REMFAA_L0002349_E0000001_517.xml',
                                       'data/L0002349_L0002349/vacio.xml',
                                       'data/L0002349_L0000001/vacio.xml']]
-        self.assertListEqual(list(ftp2email.get_message_files()), espected_files)
 
+        self.assertListEqual(list(self.ch.load_messages()), espected_files)
+
+    def test_get_message_data(self):
+        "Verifica que se pueda cargar el contenido de un mensaje"
+        msg_id = os.path.join(self.test_path, 'data/L0002349_E0000001/vacio.xml')
+        self.assertEqual(self.ch.get_message_data(msg_id), '')
+
+        msg_id = os.path.join(self.test_path,
+                                'data/L0002349_E0000001/REMFAA_L0002349_E0000001_517.xml')
+        self.assertEqual(self.ch.get_message_data(msg_id), open(msg_id).read())
+
+    def test_mark_message(self):
+        "Verifica que se mueva el archivo del mensaje para marcarlo como leido"
+
+        real_move = ftp2email.shutil.move
+        move_operations = []
+
+        def fake_move(src, dst):
+            move_operations.append((src, dst))
+        ftp2email.shutil.move = fake_move
+
+        self.ch.mark_message('/path/to/message.xml')
+        msg_id = os.path.join(self.test_path, 'data/L0002349_E0000001/vacio.xml')
+        self.ch.mark_message(msg_id)
+        self.assertEqual(move_operations,
+                            [(msg_id,
+                              os.path.join(self.test_path, 'data/L0002349_E0000001/archived/vacio.xml'))])
+        self.assertEqual(ftp2email.logging.messages['error'],
+                            ['El archivo del mensaje no existe /path/to/message.xml'])
+        self.assertEqual(ftp2email.logging.messages['info'],
+                            ['Moviendo archivo de mensaje %s' % msg_id])
+
+        ftp2email.shutil.move = real_move
 
 
 def main():
--- utils/ftp2email.py
+++ utils/ftp2email.py
@@ -1,36 +1,110 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 :
 
+import csv
+import errno
 import json
-import re
+import logging
 import os
+import re
+import shutil
 import sys
-
 
 with open('settings.json') as i:
     settings = json.load(i)
 
 
-def get_message_files():
-    """Devuelve una lista con los nombres de archivo de los mensajes
-    """
-    dir_re = re.compile(settings['dir_re'])
-    for dirpath, dirname, filenames in os.walk(settings['base_path']):
-        if dir_re.search(dirpath):
-            for filename in filenames:
-                yield os.path.join(dirpath, filename)
+class MessageChannel(object):
+    "Canal que permite enviar y recibir mensajes"
+
+    def load_messages(self):
+        "Devuelve una lista con los ids de los mensajes encontrados"
+        raise NotImplementedError
+
+    def get_message_data(self, msg_id):
+        "Devuelve el contenido del mensaje con id msg_id"
+        raise NotImplementedError
+
+    def send_message(self, msg_data):
+        ":msg_data: datos del mensaje a enviar"
+        raise NotImplementedError
 
 
-def send_message(msg):
-    """Envia por email el mensaje al destinatario Sinliarg
-    """
-    return True
+class FilesystemChannel(MessageChannel):
+    "Canal de intercambio de mensajes por sistema de archivos"
+
+    def __init__(self, base_path, dir_re):
+        """
+            :base_path: directorio base donde se guardan los mensajes
+            :dir_re: expresion regular usada para reconocer
+                     los directorios que contienen mensajes
+        """
+        self.base_path = os.path.abspath(base_path)
+        self.dir_re = re.compile(dir_re)
+
+    def get_message_data(self, msg_id):
+        """Devuelve el contenido del mensaje msg_id
+            :msg_id: el id del mensaje es el path al archivo con su contenido
+        """
+        with open(msg_id) as i:
+            return i.read()
+
+    def load_messages(self):
+        """Devuelve una lista con los nombres de archivo de los mensajes
+        """
+        for dirpath, dirname, filenames in os.walk(self.base_path):
+            if self.dir_re.search(dirpath):
+                for filename in filenames:
+                    yield os.path.join(dirpath, filename)
+
+    def mark_message(self, msg_id):
+        """Marca un mensaje como procesado/leido
+            Mueve el archivo con el mensaje a un directorio 'leidos'
+            :msg_id: path al archivo que contiene el mensaje
+        """
+        if not os.path.isfile(msg_id):
+            logging.error('El archivo del mensaje no existe %s'
+                            % msg_id)
+            return
+
+        base_dir, filename = os.path.split(msg_id)
+        archived_path = os.path.join(base_dir, 'archived')
+        try:
+            logging.info('Moviendo archivo de mensaje %s' % msg_id)
+            shutil.move(msg_id, os.path.join(archived_path, filename))
+        except IOError, e:                  # si no existia el directorio intenta crearlo
+            if e.errno == errno.ENOENT:
+                try:
+                    logging.error('Creando directorio para mensajes archivados en %s'
+                                    % archived_path)
+                    os.makedirs(archived_path)
+                except OSError, e:          # si da error porque ya existe lo ignora
+                    if e.exception.errno != errno.EEXIST:
+                        raise
+                shutil.move(msg_id, os.path.join(archived_path, filename))
+            else:
+                raise
 
 
-def mark_message(filename):
-    """Marca el archivo del mensaje como enviado moviendolo a otro directorio
-    """
-    return True
+class EmailChannel(MessageChannel):
+    "Canal de intercambio de mensajes por email"
+
+    def __init__(self, smtp_url=None, eaddress_file=None):
+        self.smtp_url = smtp_url
+        self.eaddress_file = eaddress_file
+
+    def send_message(self, msg_data):
+        """Envia por email el mensaje al destinatario Sinliarg por email
+        """
+        if not hasattr(self, 'sinli_emails'):
+            self.load_sinli_codes()
+
+    def load_sinli_codes(self):
+        "Lee las direcciones de email para cada codigo sinli de un archivo csv"
+
+        with open(self.eaddress_file) as i:
+            self.sinli_emails = dict(csv.reader(i))
+        return True
 
 
 def __main__(argv=None):
@@ -41,16 +115,20 @@ def __main__(argv=None):
     if argv is None:
         argv = sys.argv
 
-    for message_fn in get_message_files():
+    srcChannel = FilesystemChannel(settings['base_path'], settings['dir_re'])
+    dstChannel = EmailChannel(smtp_url=settings['smtp_url'], eaddress_file=settings['eaddress_file'])
+    for msg_id in srcChannel.load_messages():
         try:
-            logger.info('sending message')
-            with open(message_fn) as message_data:
-                send_message(message_data)
-            logger.info('message sent')
+            msg_data = srcChannel.get_message_data(msg_id)
         except Exception, e:
-            logger.error(e)
+            logging.error('Error obteniendo datos del mensaje\n%s' % e)
+
+        try:
+            dstChannel.send_message(msg_data)
+        except Exception, e:
+            logging.error('Error enviando mensaje\n%s' % e)
         else:
-            mark_message(message_fn)
+            srcChannel.mark_message(msg_id)
 
     return 0
 
