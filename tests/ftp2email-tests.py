874693e5d64f667e4bd18553457c56a0acf9a465
Facundo Casco <facundo.casco@fierro-soft.com.ar>
2012-10-29 18:22:11 -0300

    nueva clase para los objetos mensaje; nuevo canal para enviar mensajes por email; agregada opcion para leer el nombre del archivo de configuracion en la linea de comandos;

--- tests/ftp2email-tests.py
+++ tests/ftp2email-tests.py
@@ -5,7 +5,10 @@
 
 import os
 import sys
+import traceback
 import unittest
+
+import mock
 
 # fixme: hay una forma menos fea de incluir en el path el directorio donde esta utils?
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
@@ -33,8 +36,51 @@ class LoggingTester(object):
 ftp2email.logging = LoggingTester()
 
 
+class SinliargMessageTestCase(unittest.TestCase):
+    """Test para los mensajes"""
+
+    def setUp(self):
+        xmldata = """<?xml version="1.0" encoding="utf-8"?>
+                     <REMFAA>
+                     <ARCHIVO>
+                         <DESCRIPCION>Factura/Remito 0001-00336393</DESCRIPCION>
+                         <FECHA>2012-09-18</FECHA>
+                         <VERSION>1.0</VERSION>
+                         <CODIGO>REMFAA</CODIGO>
+                     </ARCHIVO>
+                     <ORIGEN>
+                         <NOMBRE>ILHSA</NOMBRE>
+                         <CUIT />
+                         <ID_SUCURSAL />
+                         <CODIGO_SINLI>L0002349</CODIGO_SINLI>
+                     </ORIGEN>
+                     <DESTINO>
+                         <NOMBRE>Editorial 1</NOMBRE>
+                         <CUIT>30-00000000-1</CUIT>
+                         <ID_SUCURSAL>1</ID_SUCURSAL>
+                         <CODIGO_SINLI>E0000001</CODIGO_SINLI>
+                     </DESTINO>
+                     </REMFAA>"""
+        self.message = ftp2email.SinliargMessage(xmldata)
+
+    def test_dst_code(self):
+        """Verifica el codigo sinli de destino del mensaje
+        """
+        self.assertEqual(self.message.dst_code, 'E0000001')
+
+    def test_src_code(self):
+        """Verifica el codigo sinli de origen del mensaje
+        """
+        self.assertEqual(self.message.src_code, 'L0002349')
+
+    def test_descripcion(self):
+        """Verifica la descripcion del mensaje
+        """
+        self.assertEqual(self.message.description, 'Factura/Remito 0001-00336393')
+
+
 class FilesystemChannelTestCase(unittest.TestCase):
-    "Test para el manejo de mensajes por archivos"
+    """Test para el manejo de mensajes por archivos"""
 
     @classmethod
     def setUpClass(cls):
@@ -49,8 +95,8 @@ class FilesystemChannelTestCase(unittest.TestCase):
         ftp2email.logging.flush()
 
     def test_get_message_files(self):
-        "Verifica que se reconozcan correctamente los archivos con mensajes"
-
+        """Verifica que se reconozcan correctamente los archivos con mensajes
+        """
         espected_files = [os.path.join(self.test_path, x)
                             for x in ['data/L0002349_E0000001/vacio.xml',
                                       'data/L0002349_E0000001/REMFAA_L0002349_E0000001_517.xml',
@@ -60,7 +106,8 @@ class FilesystemChannelTestCase(unittest.TestCase):
         self.assertListEqual(list(self.ch.load_messages()), espected_files)
 
     def test_get_message_data(self):
-        "Verifica que se pueda cargar el contenido de un mensaje"
+        """Verifica que se pueda cargar el contenido de un mensaje
+        """
         msg_id = os.path.join(self.test_path, 'data/L0002349_E0000001/vacio.xml')
         self.assertEqual(self.ch.get_message_data(msg_id), '')
 
@@ -68,28 +115,189 @@ class FilesystemChannelTestCase(unittest.TestCase):
                                 'data/L0002349_E0000001/REMFAA_L0002349_E0000001_517.xml')
         self.assertEqual(self.ch.get_message_data(msg_id), open(msg_id).read())
 
-    def test_mark_message(self):
-        "Verifica que se mueva el archivo del mensaje para marcarlo como leido"
-
-        real_move = ftp2email.shutil.move
-        move_operations = []
-
-        def fake_move(src, dst):
-            move_operations.append((src, dst))
-        ftp2email.shutil.move = fake_move
+    @mock.patch('%s.ftp2email.shutil.move' % __name__)
+    def test_mark_message(self, move_mock):
+        """Verifica que se mueva el archivo del mensaje para marcarlo como leido
+        """
 
         self.ch.mark_message('/path/to/message.xml')
         msg_id = os.path.join(self.test_path, 'data/L0002349_E0000001/vacio.xml')
         self.ch.mark_message(msg_id)
-        self.assertEqual(move_operations,
-                            [(msg_id,
-                              os.path.join(self.test_path, 'data/L0002349_E0000001/archived/vacio.xml'))])
+
+        move_mock.assert_called_with(msg_id,
+                                     os.path.join(self.test_path,
+                                            'data/L0002349_E0000001/archived/vacio.xml'))
+
         self.assertEqual(ftp2email.logging.messages['error'],
                             ['El archivo del mensaje no existe /path/to/message.xml'])
         self.assertEqual(ftp2email.logging.messages['info'],
                             ['Moviendo archivo de mensaje %s' % msg_id])
 
-        ftp2email.shutil.move = real_move
+
+class EmailChannelTestCase(unittest.TestCase):
+    """Test para el manejo de mensajes por email"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Los archivos para tests estan en un directorio 'data'
+            dentro del directorio donde está el test
+        """
+        cls.test_path = os.path.dirname(os.path.realpath(__file__))
+
+    def setUp(self):
+        eaddress_file = os.path.join(self.test_path, 'data/email_address.csv')
+        self.ch = ftp2email.EmailChannel(smtp_settings={'host': 'smtpserver',
+                                                        'port': None,
+                                                        'user': 'u',
+                                                        'pass': 'p'},
+                                         msg_from='test@example.com',
+                                         eaddress_file=eaddress_file)
+        ftp2email.logging.flush()
+
+    def test_load_sinli_codes(self):
+        """Verifica que se carguen correctamente los codigos y direcciones de email
+        """
+        emails = self.ch.load_sinli_codes()
+
+        self.assertEqual(len(emails), 12)
+        self.assertEqual(emails['L0001562'], 'sinli@cuspide.com')
+        self.assertEqual(emails['L0001563'], 'sinliarg@libreriahernandez.com.ar')
+        self.assertTrue('Z0000000' not in emails)
+        self.assertListEqual(emails.values(), self.ch.sinli_emails.values())
+
+    def test_get_destination_address(self):
+        """Verifica que se obtenga correctamente la direcciond de email
+            a donde se debe enviar el mensaje
+        """
+        xmldata = """<?xml version="1.0" encoding="utf-8"?>
+                     <REMFAA>
+                     <ARCHIVO>
+                         <DESCRIPCION>Factura/Remito 0001-00336393</DESCRIPCION>
+                         <FECHA>2012-09-18</FECHA>
+                         <VERSION>1.0</VERSION>
+                         <CODIGO>REMFAA</CODIGO>
+                     </ARCHIVO>
+                     <ORIGEN>
+                         <NOMBRE>ILHSA</NOMBRE>
+                         <CUIT />
+                         <ID_SUCURSAL />
+                         <CODIGO_SINLI>L0002349</CODIGO_SINLI>
+                     </ORIGEN>
+                     <DESTINO>
+                         <NOMBRE>Editorial 1</NOMBRE>
+                         <CUIT>30-00000000-1</CUIT>
+                         <ID_SUCURSAL>1</ID_SUCURSAL>
+                         <CODIGO_SINLI>E0000001</CODIGO_SINLI>
+                     </DESTINO>
+                     </REMFAA>"""
+
+        self.assertEqual(self.ch.get_destination_address(ftp2email.SinliargMessage(xmldata)),
+                         'fc@fierro-soft.com.ar')
+
+    def test_gen_email_subject(self):
+        """Verifica la generacion del asunto del email
+        """
+        xmldata = """<?xml version="1.0" encoding="utf-8"?>
+                     <REMFAA>
+                     <ARCHIVO>
+                         <DESCRIPCION>Factura/Remito 0001-00336393</DESCRIPCION>
+                         <FECHA>2012-09-18</FECHA>
+                         <VERSION>1.0</VERSION>
+                         <CODIGO>REMFAA</CODIGO>
+                     </ARCHIVO>
+                     <ORIGEN>
+                         <NOMBRE>ILHSA</NOMBRE>
+                         <CUIT />
+                         <ID_SUCURSAL />
+                         <CODIGO_SINLI>L0002349</CODIGO_SINLI>
+                     </ORIGEN>
+                     <DESTINO>
+                         <NOMBRE>Editorial 1</NOMBRE>
+                         <CUIT>30-00000000-1</CUIT>
+                         <ID_SUCURSAL>1</ID_SUCURSAL>
+                         <CODIGO_SINLI>E0000001</CODIGO_SINLI>
+                     </DESTINO>
+                     </REMFAA>"""
+
+        self.assertEqual(self.ch.gen_email_subject(ftp2email.SinliargMessage(xmldata)),
+                         'SINLIARG: Tipo: REMFAA, De: L0002349, Para: E0000001')
+
+    def test_gen_email_body(self):
+        """Verifica la generacion del cuerpo del email
+        """
+        xmldata = """<?xml version="1.0" encoding="utf-8"?>
+                     <REMFAA>
+                     <ARCHIVO>
+                         <DESCRIPCION>Factura/Remito 0001-00336393</DESCRIPCION>
+                         <FECHA>2012-09-18</FECHA>
+                         <VERSION>1.0</VERSION>
+                         <CODIGO>REMFAA</CODIGO>
+                     </ARCHIVO>
+                     <ORIGEN>
+                         <NOMBRE>ILHSA</NOMBRE>
+                         <CUIT />
+                         <ID_SUCURSAL />
+                         <CODIGO_SINLI>L0002349</CODIGO_SINLI>
+                     </ORIGEN>
+                     <DESTINO>
+                         <NOMBRE>Editorial 1</NOMBRE>
+                         <CUIT>30-00000000-1</CUIT>
+                         <ID_SUCURSAL>1</ID_SUCURSAL>
+                         <CODIGO_SINLI>E0000001</CODIGO_SINLI>
+                     </DESTINO>
+                     </REMFAA>"""
+
+        self.assertEqual(self.ch.gen_email_body(ftp2email.SinliargMessage(xmldata)),
+                            'Factura/Remito 0001-00336393')
+
+    def test_gen_filename(self):
+        """Verifica la generacion del nombre de archivo para el adjunto
+        """
+        xmldata = """<?xml version="1.0" encoding="utf-8"?>
+                     <REMFAA>
+                     <ARCHIVO>
+                         <DESCRIPCION>Factura/Remito 0001-00336393</DESCRIPCION>
+                         <FECHA>2012-09-18</FECHA>
+                         <VERSION>1.0</VERSION>
+                         <CODIGO>REMFAA</CODIGO>
+                     </ARCHIVO>
+                     <ORIGEN>
+                         <NOMBRE>ILHSA</NOMBRE>
+                         <CUIT />
+                         <ID_SUCURSAL />
+                         <CODIGO_SINLI>L0002349</CODIGO_SINLI>
+                     </ORIGEN>
+                     <DESTINO>
+                         <NOMBRE>Editorial 1</NOMBRE>
+                         <CUIT>30-00000000-1</CUIT>
+                         <ID_SUCURSAL>1</ID_SUCURSAL>
+                         <CODIGO_SINLI>E0000001</CODIGO_SINLI>
+                     </DESTINO>
+                     </REMFAA>"""
+
+        self.assertEqual(self.ch.gen_filename(ftp2email.SinliargMessage(xmldata)),
+                            'REMFAA_L0002349_E0000001_2717603300731787160.xml')
+
+    def test_send_message(self):
+        """Verifica el envio del mensaje por email
+        """
+        data_file = os.path.join(self.test_path,
+                                 'data/L0002349_E0000001/REMFAA_L0002349_E0000001_517.xml')
+        with open(data_file) as i:
+            msg_data = i.read()
+
+        with mock.patch('%s.ftp2email.smtplib.SMTP' % __name__) as smtp_mock:
+            smtpserver_mock = mock.Mock(create=True)
+            smtp_mock.return_value = smtpserver_mock
+            try:
+                self.ch.send_message(ftp2email.SinliargMessage(msg_data))
+            except Exception:
+                self.fail('Unexpected exception: %s' % traceback.format_exc())
+
+            smtp_mock.assert_called_once_with('smtpserver', port=None, timeout=5)
+            smtpserver_mock.login.assert_called_once_with('u', 'p')
+            self.assertEqual(smtpserver_mock.sendmail.call_count, 1)
+            self.assertEqual(smtpserver_mock.close.call_count, 1)
 
 
 def main():
--- utils/ftp2email.py
+++ utils/ftp2email.py
@@ -1,37 +1,58 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 :
 
+import cStringIO
 import csv
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import errno
 import json
 import logging
 import os
 import re
 import shutil
+import smtplib
 import sys
+import traceback
+import xml.etree.cElementTree as cElementTree
 
-with open('settings.json') as i:
-    settings = json.load(i)
+
+class SinliargMessage(object):
+    """Mensaje de sinliarg"""
+
+    def __init__(self, msg_data):
+        """
+            :msg_data: datos del mensaje en formato XML
+        """
+        self.xml = msg_data
+        xmltree = cElementTree.parse(cStringIO.StringIO(self.xml))
+        self.dst_code = xmltree.findtext('DESTINO/CODIGO_SINLI')
+        self.src_code = xmltree.findtext('ORIGEN/CODIGO_SINLI')
+        self.description = xmltree.findtext('ARCHIVO/DESCRIPCION')
+        self.sinli_type = xmltree.findtext('ARCHIVO/CODIGO')
 
 
 class MessageChannel(object):
-    "Canal que permite enviar y recibir mensajes"
+    """Canal que permite enviar y recibir mensajes"""
 
     def load_messages(self):
-        "Devuelve una lista con los ids de los mensajes encontrados"
+        """Devuelve una lista con los ids de los mensajes encontrados
+        """
         raise NotImplementedError
 
     def get_message_data(self, msg_id):
-        "Devuelve el contenido del mensaje con id msg_id"
+        """Devuelve el contenido del mensaje con id msg_id
+        """
         raise NotImplementedError
 
     def send_message(self, msg_data):
-        ":msg_data: datos del mensaje a enviar"
+        """:msg_data: datos del mensaje a enviar
+        """
         raise NotImplementedError
 
 
 class FilesystemChannel(MessageChannel):
-    "Canal de intercambio de mensajes por sistema de archivos"
+    """Canal de intercambio de mensajes por sistema de archivos"""
 
     def __init__(self, base_path, dir_re):
         """
@@ -87,24 +108,75 @@ class FilesystemChannel(MessageChannel):
 
 
 class EmailChannel(MessageChannel):
-    "Canal de intercambio de mensajes por email"
+    """Canal de intercambio de mensajes por email"""
 
-    def __init__(self, smtp_url=None, eaddress_file=None):
-        self.smtp_url = smtp_url
+    def __init__(self, smtp_settings, msg_from='', eaddress_file=None):
+        """
+            :smtp_settings: dict de configuracio del servidor smtp
+                            {'host': '', 'port': '', 'user': '', 'pass': ''}
+            :msg_from: valor que identifica el origen de los emails enviados
+            :eaddress_file: nombre del archivo con codigo sinli, direccion de email (csv)
+        """
+        self.smtp_settings = smtp_settings
+        self.msg_from = msg_from
         self.eaddress_file = eaddress_file
 
-    def send_message(self, msg_data):
-        """Envia por email el mensaje al destinatario Sinliarg por email
+    def send_message(self, sinli_message):
+        """Envia el mensaje al destinatario Sinliarg por email
+            :sinli_message: mensaje Sinliarg a enviar
+        """
+        # crear el email a enviar
+        dest_addr = self.get_destination_address(sinli_message)
+        new_email = MIMEMultipart()
+        new_email['From'] = self.msg_from
+        new_email['To'] = dest_addr
+        new_email['Subject'] = self.gen_email_subject(sinli_message)
+        new_email.attach(MIMEText(self.gen_email_body(sinli_message)))
+        xml_attach = MIMEText(sinli_message.xml, 'xml', 'utf-8')
+        xml_attach['Content-disposition'] = 'attachment; filename="%s"' \
+                                                % self.gen_filename(sinli_message)
+        new_email.attach(xml_attach)
+
+        # conectar con el servidor y hacer en envio
+        smtp_server = smtplib.SMTP(self.smtp_settings['host'],
+                                   port=self.smtp_settings.get('port', None), timeout=5)
+        if self.smtp_settings['user'] is not None:
+            smtp_server.login(self.smtp_settings['user'],
+                              self.smtp_settings['pass'])
+        smtp_server.sendmail(self.msg_from, dest_addr, new_email.as_string())
+        smtp_server.close()
+
+    def load_sinli_codes(self):
+        """Lee las direcciones de email para cada codigo sinli de un archivo csv
+        """
+        with open(self.eaddress_file) as i:
+            self.sinli_emails = dict(csv.reader(i))
+        return self.sinli_emails
+
+    def get_destination_address(self, message):
+        """Devolve la direccion de email a donde debe ser enviado el mensaje
+            :message: mensaje Sinliarg del cual se busca la direccion destino
         """
         if not hasattr(self, 'sinli_emails'):
             self.load_sinli_codes()
+        return self.sinli_emails[message.dst_code]
 
-    def load_sinli_codes(self):
-        "Lee las direcciones de email para cada codigo sinli de un archivo csv"
+    def gen_email_subject(self, message):
+        """Genera una linea para asunto del email a enviar con el mensaje
+        """
+        return 'SINLIARG: Tipo: %s, De: %s, Para: %s' \
+                % (message.sinli_type, message.src_code, message.dst_code)
 
-        with open(self.eaddress_file) as i:
-            self.sinli_emails = dict(csv.reader(i))
-        return True
+    def gen_email_body(self, message):
+        """Genera el texto para el cuerpo del email a enviar con el mensaje
+        """
+        return message.description or 'Mensaje sinliarg adjunto'
+
+    def gen_filename(self, message):
+        """Genera un nombre de archivo para el contenido del mensaje
+        """
+        return '%s.xml' % '_'.join((message.sinli_type, message.src_code,
+                                    message.dst_code, str(hash(message.xml))))
 
 
 def __main__(argv=None):
@@ -115,20 +187,48 @@ def __main__(argv=None):
     if argv is None:
         argv = sys.argv
 
+    if len(argv) > 1:
+        settings_filename = argv[1]
+    else:
+        settings_filename = 'settings.json'
+
+    try:
+        with open(settings_filename) as i:
+            settings = json.load(i)
+    except Exception:
+        print 'Error abriendo archivo de configuracion: %s\n\n' % settings_filename
+        raise
+
+    log_format = '%(asctime)s|%(levelname)s|%(message)s'
+    if 'log_file' in settings:
+        logging.basicConfig(filename=settings['log_file'], level=logging.DEBUG,
+                            format=log_format)
+    else:
+        logging.basicConfig(level=logging.DEBUG, format=log_format)
+
     srcChannel = FilesystemChannel(settings['base_path'], settings['dir_re'])
-    dstChannel = EmailChannel(smtp_url=settings['smtp_url'], eaddress_file=settings['eaddress_file'])
+    dstChannel = EmailChannel(smtp_settings=settings['smtp_settings'],
+                              msg_from='testsinli@fierro-soft.com.ar',
+                              eaddress_file=settings['eaddress_file'])
+
+    logging.info('Envio de mensajes FTP->Email iniciado')
     for msg_id in srcChannel.load_messages():
+        logging.info('Procesando mensaje id: %s' % msg_id)
         try:
             msg_data = srcChannel.get_message_data(msg_id)
-        except Exception, e:
-            logging.error('Error obteniendo datos del mensaje\n%s' % e)
+            logging.debug('...leido correctamente')
+        except Exception:
+            logging.error('Error obteniendo datos del mensaje\n%s' % traceback.format_exc())
 
         try:
-            dstChannel.send_message(msg_data)
-        except Exception, e:
-            logging.error('Error enviando mensaje\n%s' % e)
+            logging.info('Enviando mensaje id: %s' % msg_id)
+            dstChannel.send_message(SinliargMessage(msg_data))
+            logging.debug('...enviado correctamente')
+        except Exception:
+            logging.error('Error enviando mensaje\n%s' % traceback.format_exc())
         else:
             srcChannel.mark_message(msg_id)
+    logging.info('Envio de mensajes FTP->Email finalizado')
 
     return 0
 
